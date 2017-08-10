e7f2652cc5699a5b46befd139ccfbbd7de77dc4e
Elvis Dedic <elvis26112009@gmail.com>
2017-08-10 20:32:07 +0200

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
+    }
+}

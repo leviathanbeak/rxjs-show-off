8d79ebd246c03b219ad23976c2179dfcd70846b5
Elvis Dedic <elvis26112009@gmail.com>
2017-08-13 15:29:20 +0200

    moving code to other files

--- src/app.js
+++ src/app.js
@@ -1,41 +1,4 @@
-import Rx from 'rxjs/Rx'
-import getSubscriber from './getSubscriber'
-import * as observables from './observables'
-
-const input = document.getElementById('input')
-const button = document.getElementById('button')
-const coords = document.getElementById('coords')
-
-const buttonStream$ = Rx.Observable.fromEvent(button, 'click')
-
-buttonStream$.subscribe(x => {
-    console.log('Clicked')
-}, err => {
-    console.log(err)
-}, () => {
-    console.log('completed')
-})
-
-const inputStream$ = Rx.Observable.fromEvent(input, 'keyup')
-
-inputStream$.subscribe(x => {
-    console.log(x)
-})
-
-const mouseMove$ = Rx.Observable.fromEvent(document, 'mousemove')
-
-mouseMove$.subscribe(x => {
-    coords.innerHTML = `X: ${x.clientX} - Y: ${x.clientY}`
-})
-
-const nums = [1, 2, 3, 4, 5, 6, 7]
-
-Rx.Observable.from(nums).subscribe(getSubscriber('nums'))
-
-const map = new Map([
-    [1, 2],
-    [3, 4],
-    [5, 6]
-])
-
-Rx.Observable.from(map).subscribe(getSubscriber('map'))
+//import * as events from './events'
+//import * as observables from './observables'
+//import * as operators from './operators'
+import * as promises from './promises'

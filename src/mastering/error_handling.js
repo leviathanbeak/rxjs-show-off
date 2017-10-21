0d366fc6eaa15a098be514a44d41903900e33b97
Elvis Dedic <elvis26112009@gmail.com>
2017-10-21 16:59:45 +0200

    styling comment

--- src/mastering/error_handling.js
+++ src/mastering/error_handling.js
@@ -13,7 +13,8 @@ let caught = getJSON([`{"1": 1}`, `{"1: 1}`]).catch(
 
 caught.subscribe(json => console.log(json), e => console.log('ERROR', e.message))
 
-//In the preceding code, we create a new Observable, caught, that uses the catch operator to catch errors in the original Observable. If there’s an error it will continue the sequence with an Observable that emits only one item, with an error property describing the error.
+//In the preceding code, we create a new Observable, caught, that uses the catch operator to catch errors in the original Observable. 
+//If there’s an error it will continue the sequence with an Observable that emits only one item, with an error property describing the error.
 
 //retrying if it fails
 Observable.ajax({ method: 'GET', url: 'https://api.github.com/users/leviathan88' }).retry(5).subscribe(d => console.log(d))

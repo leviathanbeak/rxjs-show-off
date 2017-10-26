4d9213eb1be3b2a6b475d8181d7a3a219f7629ea
Elvis Dedic <elvis26112009@gmail.com>
2017-10-26 19:50:52 +0200

    add play

---
+++ src/RxJS/play.js
@@ -0,0 +1,3 @@
+import { Observable } from 'rxjs/Rx'
+
+Observable.of([1, 23, 3, 5, 6]).subscribe(s => console.log(s))
--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
 import * as learningCanvas from './canvas/01-paths-and-text'
+//import * as justPlay from './RxJS/play'
 console.log('just start')

f0b7de1983580423e8007a963b310b32d0fc22c1
Elvis Dedic <elvis26112009@gmail.com>
2017-10-29 09:49:05 +0100

    lines

--- src/canvas/cookbook/paths and text/horizontal, vertical, assoreted.js
+++ src/canvas/cookbook/paths and text/horizontal, vertical, assoreted.js
@@ -1,11 +1,7 @@
-require('./style.css')
-import { Observable } from 'rxjs/Rx'
-
 const canvas = document.createElement('canvas')
 canvas.setAttribute('id', 'canvas')
 const ctx = canvas.getContext('2d')
 document.body.appendChild(canvas)
-
 
 function drawLine({ color, width, cap, xstart, ystart, xend, yend }) {
 
@@ -22,8 +18,48 @@ function drawLine({ color, width, cap, xstart, ystart, xend, yend }) {
 
 const parametrs = { color: 'black', xstart: 0, ystart: 0, xend: 500, yend: 50, width: 10, cap: 'square' }
 
-drawLine(parametrs)
+//drawLine(parametrs)
 
-// Observable.interval(1000).subscribe(x => {
-//     console.log(x)
-// })
+function drawVerticalLines() {
+    const o = {
+        color: 'crimson',
+        xstart: 130,
+        ystart: 10,
+        xend: 130,
+        yend: 60,
+        width: 2,
+        cap: 'square'
+    }
+
+    for (let i = 0; i < 10; i++) {
+        o.xstart += 15
+        o.xend += 15
+        drawLine(o)
+    }
+}
+
+drawVerticalLines()
+
+function drawHorizontalLines() {
+    const o = {
+        color: 'teal',
+        xstart: 10,
+        ystart: 10,
+        xend: 100,
+        yend: 10,
+        width: 2,
+        cap: 'square'
+    }
+
+    for (let i = 0; i < 10; i++) {
+        o.ystart += 15
+        o.yend += 15
+        drawLine(o)
+    }
+}
+
+drawHorizontalLines()
+
+function drawAssortedLines() {
+    // too stupid
+}

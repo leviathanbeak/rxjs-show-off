940dd8228ad76c3c9adcfdd07d6b631065bae96b
Elvis Dedic <elvis26112009@gmail.com>
2017-11-07 21:19:23 +0100

    remove log

--- src/app.js
+++ src/app.js
@@ -1,3 +1,2 @@
-//import * as canvasPlay from './canvas/play'
-import * as justPlay from './RxJS/play'
-console.log('just start')
+import * as canvasPlay from './canvas/play'
+//import * as justPlay from './RxJS/play'
